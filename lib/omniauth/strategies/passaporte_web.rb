31f7ecf4ea72df80a2517772ed357430a5c09196
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2016-07-26 15:48:14 -0300

    Fetch raw_info from the service

--- lib/omniauth/strategies/passaporte_web.rb
+++ lib/omniauth/strategies/passaporte_web.rb
@@ -5,8 +5,6 @@ module OmniAuth
 
       option :name, :passaporte_web
 
-      # This is where you pass the options you would pass when
-      # initializing your consumer from the OAuth gem.
       option :client_options, {
         :site => 'https://app.passaporteweb.com.br',
         :authorize_path => '/oauth/authorize',
@@ -15,19 +13,15 @@ module OmniAuth
         :raise_errors => true,
       }
 
-      # These are called after authentication has succeeded. If
-      # possible, you should try to set the UID without making
-      # additional calls (if the user id is returned with the token
-      # or as a URI parameter). This may not be possible with all
-      # providers.
       uid do
         raw_info['id']
       end
 
       info do
         {
+          :id => raw_info['id'],
           :name => raw_info['name'],
-          :email => raw_info["email"]
+          :email => raw_info['email'],
         }
       end
 
@@ -37,16 +31,29 @@ module OmniAuth
         }
       end
 
+      # Example:
+      #
+      #   {
+      #     "id"=>"e9fa918b-a90e-49f3-86ec-e3ce92488a3e",
+      #     "name"=>"John Doe",
+      #     "email"=>"john@doe.com",
+      #     "created_at"=>"2016-07-21T22:02:17Z",
+      #     "updated_at"=>"2016-07-21T22:02:17Z",
+      #     "_links"=>{
+      #       "self"=>{
+      #         "href"=>"https://app.passaporteweb.com.br/api/v1/users/e9fa918b-a90e-49f3-86ec-e3ce92488a3e"
+      #       }
+      #     }
+      #   }
       def raw_info
-        # @raw_info ||= access_token.get('/api/v1/profile').parsed
-        {'id' => 42, 'name' => 'Quarenta e Dois', 'email' => 'qua@renta.dois'}
+        @raw_info ||= access_token.get('/api/v1/profile').parsed
       end
 
       def request_phase
         options[:authorize_params] = {
-          :client_id     => options['client_id'],
+          :client_id => options['client_id'],
           :response_type => 'code',
-          :scopes        => (options['scope'] || DEFAULT_SCOPE)
+          :scopes => (options['scope'] || DEFAULT_SCOPE)
         }
         super
       end
