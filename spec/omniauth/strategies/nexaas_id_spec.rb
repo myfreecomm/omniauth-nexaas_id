827a6c4a30673ec98e230a97bd9290c2273d3db1
Rodrigo Cacilhas <rodrigo.cacilhas@nexaas.com>
2018-10-11 16:25:37 -0300

    Removed api_token attribute (only @api_token for now)

--- spec/omniauth/strategies/nexaas_id_spec.rb
+++ spec/omniauth/strategies/nexaas_id_spec.rb
@@ -27,8 +27,4 @@ describe OmniAuth::Strategies::NexaasID do
   it 'has a site' do
     expect(subject.options[:client_options][:site]).to eq('https://sandbox.id.nexaas.com')
   end
-
-  it 'has a nil api_token before build token' do
-    expect(subject.api_token).to be_nil
-  end
 end
