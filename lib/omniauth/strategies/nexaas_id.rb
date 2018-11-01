e6a839c462c3760d7486976ce62704bc5b15ae9b
Rodrigo Cacilhas <rodrigo.cacilhas@nexaas.com>
2018-11-01 14:11:49 -0300

    Remove obsolete comment

--- lib/omniauth/strategies/nexaas_id.rb
+++ lib/omniauth/strategies/nexaas_id.rb
@@ -39,21 +39,6 @@ module OmniAuth
         }
       end
 
-      # Example:
-      #
-      #   {
-      #     "id"=>"e9fa918b-a90e-49f3-86ec-e3ce92488a3e",
-      #     "full_name"=>"John Doe",
-      #     "email"=>"john@doe.com",
-      #     "emails"=>[{"address"=>"john@doe.com", "confirmed"=>true}],
-      #     "created_at"=>"2016-07-21T22:02:17Z",
-      #     "updated_at"=>"2016-07-21T22:02:17Z",
-      #     "_links"=>{
-      #       "self"=>{
-      #         "href"=>"https://id.nexaas.com/api/v1/users/e9fa918b-a90e-49f3-86ec-e3ce92488a3e"
-      #       }
-      #     }
-      #   }
       def raw_info
         @raw_info ||= access_token.get('/api/v1/profile').parsed
       end
