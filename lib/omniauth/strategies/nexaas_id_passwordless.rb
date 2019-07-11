b7e9fcfa7567e743098b2de87ed4b4d7fe5ef452
Icaro Ang Siqueira <siqueira.icaro@gmail.com>
2019-07-11 10:21:47 -0300

    Create NexaasIdPasswordless strategy

---
+++ lib/omniauth/strategies/nexaas_id_passwordless.rb
@@ -0,0 +1,91 @@
+require 'omniauth-oauth2'
+
+OmniAuth.config.add_camelization('nexaas_id_passwordless', 'NexaasIDPasswordless')
+
+module OmniAuth
+  module Strategies
+    class NexaasIDPasswordless < OmniAuth::Strategies::OAuth2
+      DEFAULT_SCOPE = 'profile invite'.freeze
+
+      def initialize(*args)
+        @api_token = nil
+        super
+      end
+
+      option :name, :nexaas_id_passwordless
+      option :client_options, site: 'https://id.nexaas.com',
+                              authorize_url: 'oauth/passwordless/authorize'
+      option :list_emails, false
+
+      uid do
+        raw_info['id']
+      end
+
+      info do
+        main_email = raw_info['email']
+        {
+          id: raw_info['id'],
+          name: {
+            first: raw_info['first_name'],
+            last: raw_info['last_name']
+          },
+          fullname: raw_info['full_name'],
+          email: main_email,
+          emails: raw_info['emails'] || [main_email],
+          picture_url: raw_info['picture']
+        }
+      end
+
+      extra do
+        {
+          raw_info: raw_info,
+          legacy: { api_token: @api_token }
+        }
+      end
+
+      def raw_info
+        @raw_info ||= build_raw_info
+      end
+
+      def request_phase
+        options[:authorize_params][:scopes] = options['scope'] || DEFAULT_SCOPE
+        super
+      end
+
+      def list_emails?
+        options[:list_emails] || options['list_emails'] ||
+          options[:client_options][:list_emails] # TODO: remove this line
+      end
+
+      protected
+
+      def build_access_token
+        if (token = super) && token.params
+          @api_token = token.params['api_token']
+        end
+        token
+      end
+
+      def build_raw_info
+        add_email_list_to(access_token.get('/api/v1/profile').parsed)
+      end
+
+      def add_email_list_to(acc)
+        acc['emails'] = retrieve_emails(acc['id'])
+        acc
+      end
+
+      def retrieve_emails(id)
+        return unless list_emails? # guard: access endpoint only if allowed
+        emails = access_token.get('/api/v1/profile/emails').parsed
+        got = emails['id']
+        raise "unexpected id #{got} retrieving e-mails for #{id}" unless got == id
+        emails['emails']
+
+      rescue StandardError => err
+        warn(err)
+        nil
+      end
+    end
+  end
+end
---
+++ spec/omniauth/strategies/nexaas_id_passwordless_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe OmniAuth::Strategies::NexaasIDPasswordless do
+  subject do
+    described_class.new(
+      'app_id',
+      'app_secret',
+      client_options: { site: 'https://sandbox.id.nexaas.com',
+                        passwordless_token: 'token-123' },
+      list_emails: true
+    )
+  end
+
+  it 'has a default scope' do
+    expect(described_class::DEFAULT_SCOPE).to eq('profile invite')
+  end
+
+  it 'us a subclass of OmniAuth::Strategies::OAuth2' do
+    expect(described_class.superclass).to eq(OmniAuth::Strategies::OAuth2)
+  end
+
+  it 'has app' do
+    expect(subject.app).to eq('app_id')
+  end
+
+  it 'has a name' do
+    expect(subject.name).to eq(:nexaas_id_passwordless)
+  end
+
+  it 'has a site' do
+    expect(subject.options[:client_options][:site]).to eq('https://sandbox.id.nexaas.com')
+  end
+
+  it 'must be true list_emails' do
+    expect(subject.list_emails?).to be_truthy
+  end
+
+  it 'has a passwordless_token' do
+    expect(subject.options[:client_options][:passwordless_token]).to eq('token-123')
+  end
+
+  it 'has a authorize_url default' do
+    expect(subject.options[:client_options][:authorize_url]).to eq('oauth/passwordless/authorize')
+  end
+end
